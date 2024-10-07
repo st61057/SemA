14ede95e7e9adb3aa04c8dd8482d9ad730ba6108
st61057 <st61057@upce.cz>
2024-10-07 22:51:40 +0200

    init

---
+++ src/test/java/org/example/sema/SemAApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
