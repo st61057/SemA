5971a0d6924238cd45c3d366add553cd2ea891c6
st61057 <st61057@upce.cz>
2024-10-14 19:46:50 +0200

    fix

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -3,23 +3,26 @@ package org.example.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@AllArgsConstructor
+
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig {
 
-//    private final JwtTokenUtil jwtTokenUtil;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(@Lazy JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     private static final String[] IGNORED_PATHS = new String[]{"/v3/**", "/swagger-ui/**", "/public/**"};
 
@@ -28,15 +31,16 @@ public class SecurityConfig {
         return (web) -> web.ignoring().antMatchers(IGNORED_PATHS);
     }
 
-//
-//    @Bean
-//    public JwtAuthenticationFilter authenticationTokenFilterBean() {
-//        return new JwtAuthenticationFilter();
-//    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable().authorizeHttpRequests(request -> request.antMatchers(IGNORED_PATHS).permitAll().anyRequest().authenticated()).httpBasic().and()/*.addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)*/.build();
+        return http.csrf()
+                .disable()
+                .authorizeHttpRequests(request -> request.antMatchers(IGNORED_PATHS).permitAll().anyRequest().authenticated())
+                .httpBasic()
+                .and()
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
